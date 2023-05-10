ca121767390a55e5ed29d1a505256a6bb80e5491
Kamil Poncyliusz <kamil.poncyliusz@gmail.com>
2023-05-10 22:12:54 +0200

    Fix get-world-data-state not assigning available world data files correctly

--- ts/src/get-world-data-state.ts
+++ ts/src/get-world-data-state.ts
@@ -1,6 +1,6 @@
 import daysFromStart from "./days-from-start.js";
 import findWorldDataFiles from "./find-world-data-files.js";
-import { WorldWithWorldData, WorldDataState } from "../public/scripts/Types";
+import { WorldWithWorldData, WorldDataState } from "../Types.js";
 
 const getWorldDataState = function (
   worldsWithWorldData: WorldWithWorldData[],
@@ -23,7 +23,12 @@ const getWorldDataState = function (
     for (const worldDataTurn of world.world_data) {
       const currentTurn = addedWorld.turns[worldDataTurn.turn];
       if (currentTurn) currentTurn.id = worldDataTurn.id;
-      if (worldDataFiles[world.id][worldDataTurn.turn] === true) currentTurn.hasFiles = true;
+    }
+    const turnsWithFiles = worldDataFiles[world.id];
+    if (turnsWithFiles) {
+      for (const turn in turnsWithFiles) {
+        if (turnsWithFiles[turn] === true && addedWorld.turns[turn]) addedWorld.turns[turn].hasFiles = true;
+      }
     }
     state.push(addedWorld);
   }
